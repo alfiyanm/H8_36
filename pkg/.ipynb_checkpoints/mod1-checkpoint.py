4caaf86f005e39fc0d4b97a0c84777f4501b5709
Biggerthan5 <muchlas12@gmail.com>
2020-08-16 16:35:20 +0700

    Add Sesi3, Add Custom Module and Package

---
+++ pkg/.ipynb_checkpoints/mod1-checkpoint.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def module1():
+    print('Dari Module 1')
+
---
+++ pkg/.ipynb_checkpoints/mod2-checkpoint.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def module2():
+    print('Dari Module 2')
+
